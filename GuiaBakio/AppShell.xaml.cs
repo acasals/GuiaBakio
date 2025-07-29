c7811fa610e4c47793254a1ccbe6eba6ffda0360
Alex Casals <personal@drcasals.com>
2025-07-29 20:41:15 +0200

    En AppShell.xaml: añadida LocalidadPage

--- GuiaBakio/AppShell.xaml.cs
+++ GuiaBakio/AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace GuiaBakio
+﻿using GuiaBakio.Pages;
+
+namespace GuiaBakio
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute("localidadPage", typeof(LocalidadPage));
         }
     }
 }
