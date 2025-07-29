ac3fd4d57bbf8a57e591af278e878738b119f72c
Alex Casals <personal@drcasals.com>
2025-07-29 20:39:59 +0200

    En App.xaml.cs: añadido provvedor de servicios

--- GuiaBakio/App.xaml.cs
+++ GuiaBakio/App.xaml.cs
@@ -2,11 +2,14 @@
 {
     public partial class App : Application
     {
-        public App()
+        public static IServiceProvider Services { get; private set; }
+
+        public App(IServiceProvider services)
         {
             InitializeComponent();
 
             MainPage = new AppShell();
+            Services = services;
         }
     }
 }
