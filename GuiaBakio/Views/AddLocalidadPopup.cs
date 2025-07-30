346b2ba7e5670581b0abb14952fe3b2614ffc261
Alex Casals <personal@drcasals.com>
2025-07-30 16:34:51 +0200

    Encapsulado mejor AddLocalidadPopup

--- GuiaBakio/Views/AddLocalidadPopup.cs
+++ GuiaBakio/Views/AddLocalidadPopup.cs
@@ -4,46 +4,16 @@ namespace GuiaBakio.Views
 {
     internal class AddLocalidadPopup
     {
-       
-        private Entry? _entry;
-        private readonly TaskCompletionSource<string?> _tcs = new();
-
-        private CommunityToolkit.Maui.Views.Popup popup = new() { Color=Colors.White};
-        public Task<string?> MostrarYEsperarAsync(Page parent)
+        public Task<string?> MostrarYEsperarAsync(Page hostPage)
         {
-            MostrarPopup(parent); 
-
-            return _tcs.Task;
-        }
-
-        public void Cerrar(string nombreLocalidad)
-        {
-           _tcs?.TrySetResult(nombreLocalidad);
-            popup?.Close();
-        }
-
-
-        public void MostrarPopup(Page hostPage)
-        {
-         
-            var miTabla = new Grid
-            {
-                ColumnDefinitions =
-                {
-                    new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) },
-                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
-                },
-                RowDefinitions =
-                {
-                    new RowDefinition { Height = GridLength.Auto }
-                }
-            };
-
-            _entry = new Entry
+            var tcs = new TaskCompletionSource<string?>();
+            var popup = new CommunityToolkit.Maui.Views.Popup { Color = Colors.White };
+              
+            var entry = new Entry
             {
                 Placeholder = "Introduce una localidad",
                 HorizontalOptions = LayoutOptions.Fill,
-                VerticalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.Fill
             };
 
             var button = new Button
@@ -52,22 +22,36 @@ namespace GuiaBakio.Views
                 HorizontalOptions = LayoutOptions.Fill,
                 Command = new Command(() =>
                 {
-                    Cerrar(_entry.Text);
+                    tcs.TrySetResult(entry.Text);
+                    popup.Close();
                 })
             };
 
-            miTabla.Add(_entry, 0, 0);
-            miTabla.Add(button, 1, 0);
-
-            popup.Content = new Frame
+            var grid = new Grid
             {
-                Content = miTabla,
+                ColumnDefinitions =
+            {
+                new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) },
+                new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
+            }
+            };
+
+            grid.Add(entry, 0, 0);
+            grid.Add(button, 1, 0);
+
+            var frame = new Frame
+            {
+                Content = grid,
                 WidthRequest = 300,
                 Padding = 10,
                 CornerRadius = 12,
                 BackgroundColor = Colors.White
             };
+
+            popup.Content = frame;
             hostPage.ShowPopup(popup);
+
+            return tcs.Task;
         }
     }
 }
