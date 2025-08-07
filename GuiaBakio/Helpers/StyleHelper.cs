467a5c54b602289b118ba8ff56fe67732676b86c
Alex Casals <personal@drcasals.com>
2025-08-07 17:40:09 +0200

    En MisEstilos añadido estilo BotonCancelarStyle y añadida clase StyleHelper

---
+++ GuiaBakio/Helpers/StyleHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiaBakio.Helpers
+{
+    public static class StyleHelper
+    {
+        public static void ApplyStyle(VisualElement element, string styleKey)
+        {
+            if (Application.Current?.Resources.TryGetValue(styleKey, out var styleObj) == true && styleObj is Style style)
+            {
+                element.Style = style;
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine($"⚠️ Estilo '{styleKey}' no encontrado.");
+            }
+        }
+    }
+}
