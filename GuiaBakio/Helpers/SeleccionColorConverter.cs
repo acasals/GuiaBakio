d6b56b8a27fdce3d4c359c7051813cb15823599b
alexcasals <personal@drcasals.com>
2025-08-30 18:56:17 +0200

    Añadido EtiquetaChipStyle a MisEstilos Añadido Helper SeleccionColorConverter Añadido font MaterialSymbolsOutlined

---
+++ GuiaBakio/Helpers/SeleccionColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiaBakio.Helpers
+{
+    public class SeleccionColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isSelected = (bool)value;
+            return isSelected ? Color.FromArgb("#EF6C00") : Color.FromArgb("#FFA726");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
