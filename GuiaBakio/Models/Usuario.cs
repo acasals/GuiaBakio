26bcb09d44f798c56a327e385a9d71b5be9fe0a4
alexcasals <personal@drcasals.com>
2025-09-12 09:11:10 +0200

    Pequeñas correcciones en Usuario y NotaEtiqueta (añadida fechaModificacion)

--- GuiaBakio/Models/Usuario.cs
+++ GuiaBakio/Models/Usuario.cs
@@ -7,18 +7,21 @@ namespace GuiaBakio.Models
         [PrimaryKey]
         public string Id { get; set; }
         public string Nombre { get; set; }
-
+        public bool Sincronizado { get; set; } = false;
+        public DateTime FechaModificacion { get; set; }
         public Usuario(string nombre)
         {
             if (string.IsNullOrWhiteSpace(nombre))
                 throw new ArgumentNullException(nameof(nombre), "El Id de la nota no puede estar vacío.");
             Id = Guid.NewGuid().ToString();
             Nombre = nombre;
+            FechaModificacion = DateTime.UtcNow;
         }
         public Usuario()
         {
             Id = Guid.NewGuid().ToString();
             Nombre = string.Empty;
+            FechaModificacion = DateTime.UtcNow;
         }
     }
 }
