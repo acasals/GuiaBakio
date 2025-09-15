8f47909a4b00ec64e3d93ec40d9e1902ff174ee0
alexcasals <personal@drcasals.com>
2025-09-15 20:55:04 +0200

    En TipoEntidad: añadidas Foto, Etiqueta, NotaEtiqueta y Usuario

--- GuiaBakio/Models/TipoEntidad.cs
+++ GuiaBakio/Models/TipoEntidad.cs
@@ -3,6 +3,10 @@
     public enum TipoEntidad
     {
         Localidad = 0,
-        Nota = 1
+        Nota = 1,
+        Foto = 2,
+        Etiqueta = 3,
+        NotaEtiqueta = 4,
+        Usuario = 5
     }
 }
