55266d4e2c78e606d38a124b5b6b273dc0e528e5
alexcasals <personal@drcasals.com>
2025-12-09 00:59:44 +0100

    Añadido LocalidadTemplateSelector

---
+++ GuiaBakio/Helpers/LocalidadTemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using GuiaBakio.Models;
+
+namespace GuiaBakio.Helpers
+{
+    public class LocalidadTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate LocalidadTemplate { get; set; }
+        public DataTemplate AddButtonTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item is Localidad loc && loc.IsButton)
+                return AddButtonTemplate;
+
+            return LocalidadTemplate;
+        }
+    }
+}
