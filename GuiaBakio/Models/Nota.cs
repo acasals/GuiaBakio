7157bd6791c9618edd197f07c6b8c04d6042a69b
alexcasals <personal@drcasals.com>
2026-02-11 03:07:51 +0100

    En Nota.cs: corregido pequeño error

--- GuiaBakio/Models/Nota.cs
+++ GuiaBakio/Models/Nota.cs
@@ -15,7 +15,7 @@ namespace GuiaBakio.Models
         public Nota(string titulo, string usuarioId, string texto = "")
         {
             if (string.IsNullOrWhiteSpace(titulo))
-                throw new ArgumentNullException(nameof(titulo), "El Id de la nota no puede estar vacío.");
+                throw new ArgumentNullException(nameof(titulo), "El título de la nota no puede estar vacío.");
             if (string.IsNullOrWhiteSpace(usuarioId))
                 throw new ArgumentNullException(nameof(usuarioId), "El Id del creador no puede estar vacío.");
             Id = Guid.NewGuid().ToString();
