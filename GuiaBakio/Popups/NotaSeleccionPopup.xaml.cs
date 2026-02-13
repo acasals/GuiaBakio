1e06dc9dfa55d90ebf390647e062a6bd662d6949
alexcasals <personal@drcasals.com>
2026-02-13 01:53:45 +0100

    En NotaSeleccionPopup.xaml: añadido TapGestureRecognizer y cambiado boton de cancelar En NotaSeleccionPopup.xaml.cs: añadido evento OnItemTapped

--- GuiaBakio/Popups/NotaSeleccionPopup.xaml.cs
+++ GuiaBakio/Popups/NotaSeleccionPopup.xaml.cs
@@ -21,6 +21,32 @@ public partial class NotaSeleccionPopup : Popup
     public event EventHandler? Aceptado;
     public event EventHandler? Cancelado;
 
+    private Border? _ultimoSeleccionado;
+
+    private void OnItemTapped(object sender, TappedEventArgs e)
+    {
+        if (sender is Border border && border.BindingContext is Nota nota)
+        {
+            NotasView.SelectedItem = nota;
+
+            // 2. Restaurar el color del anterior seleccionado
+            if (_ultimoSeleccionado != null)
+            {
+                _ultimoSeleccionado.BackgroundColor = Color.FromArgb("#fafafa");
+                _ultimoSeleccionado.Stroke = Color.FromArgb("#dddddd");
+            }
+
+            // 3. Aplicar color de selección al nuevo
+            border.BackgroundColor = Color.FromArgb("#cce5ff"); // azul suave
+            border.Stroke = Color.FromArgb("#3399ff");
+
+            // 4. Guardar referencia
+            _ultimoSeleccionado = border;
+
+
+        }
+    }
+
     private void OnAceptarClicked(object sender, EventArgs e)
     {
         Aceptado?.Invoke(this, EventArgs.Empty);
